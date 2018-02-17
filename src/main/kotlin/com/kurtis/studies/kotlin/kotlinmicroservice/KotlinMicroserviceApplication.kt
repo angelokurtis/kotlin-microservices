bf434ba5b90ab4440fd6d5108f05dbaf66a95008
Tiago Angelo <tangelodev@gmail.com>
2018-02-17 16:21:47 -0200

    Initial project structure

---
+++ src/main/kotlin/com/kurtis/studies/kotlin/kotlinmicroservice/KotlinMicroserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.kurtis.studies.kotlin.kotlinmicroservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMicroserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinMicroserviceApplication>(*args)
+}
